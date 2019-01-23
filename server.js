400849f05f8e52470d1e6b6ddf7cae27c8bedae7
henry dewey <hdewey02@gmail.com>
2019-01-23 16:50:41 -0600

    first commit

--- server.js
+++ server.js
@@ -6,22 +6,25 @@ var path    = require('path')
 
 var io = require('socket.io')(http);
 
-app.use(express.static('public'))
+// app.use(express.static('public'))
+
+app.get('/', function(req, res) {
+  res.render('index');
+});
+
+
+// Chat
 
 io.on('connection', function(socket){
   console.log('a user connected');
   socket.on('disconnect', function(){
     console.log('user disconnected');
   });
-});
 
-io.on('connection', function(socket){
   socket.on('chat message', function(msg){
     console.log('message: ' + msg);
   });
-});
 
-io.on('connection', function(socket){
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
   });
